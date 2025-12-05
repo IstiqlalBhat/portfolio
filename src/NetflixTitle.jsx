a0df0bae539b69d1e38fdb98f652e1cc172f554c
Istiqlal Aurangzeb <istiqlal.aurangzeb@gmail.com>
2025-12-04 23:45:45 -0500

    Add Netflix sound and update navbar profile icon to be circular

--- src/NetflixTitle.jsx
+++ src/NetflixTitle.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './NetflixTitle.css';
-// import netflixSound from './netflix-sound.mp3'; // TODO: Add sound file
+import netflixSound from '../sound/error.mp3';
 import { useNavigate } from 'react-router-dom';
 // import logoImage from '../src/images/logo-2.png'; // TODO: Add logo image
 
@@ -9,8 +9,8 @@ const NetflixTitle = () => {
     const navigate = useNavigate();
 
     const handlePlaySound = () => {
-        // const audio = new Audio(netflixSound);
-        // audio.play().catch(error => console.error("Audio play error:", error));
+        const audio = new Audio(netflixSound);
+        audio.play().catch(error => console.error("Audio play error:", error));
         setIsClicked(true); // Starts animation after clicking
     };
 
