b34be54304892f69f9ab7da9dc41c0c940a005de
Istiqlal Aurangzeb <istiqlal.aurangzeb@gmail.com>
2025-12-05 00:12:28 -0500

    Replace DatoCMS client with mock implementation

--- src/queries/datoCMSClient.js
+++ src/queries/datoCMSClient.js
@@ -1,13 +1,19 @@
-import { GraphQLClient } from 'graphql-request';
-import { getDatoCmsToken } from './getDatoCmsToken';
+// import { GraphQLClient } from 'graphql-request';
+// import { getDatoCmsToken } from './getDatoCmsToken';
 
-const DATO_CMS_ENDPOINT = 'https://graphql.datocms.com/';
-const DATO_CMS_API_TOKEN = getDatoCmsToken();
+// const DATO_CMS_ENDPOINT = 'https://graphql.datocms.com/';
+// const DATO_CMS_API_TOKEN = getDatoCmsToken();
 
-const datoCMSClient = new GraphQLClient(DATO_CMS_ENDPOINT, {
-    headers: {
-        Authorization: `Bearer ${DATO_CMS_API_TOKEN}`,
-    },
-});
+// const datoCMSClient = new GraphQLClient(DATO_CMS_ENDPOINT, {
+//     headers: {
+//         Authorization: `Bearer ${DATO_CMS_API_TOKEN}`,
+//     },
+// });
+
+const datoCMSClient = {
+    request: async () => {
+        throw new Error("No database connected");
+    }
+};
 
 export default datoCMSClient;
