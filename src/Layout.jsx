5cf1b97975645ea4701498b0dea0d7b9207a7826
Istiqlal Aurangzeb <istiqlal.aurangzeb@gmail.com>
2025-12-05 03:15:08 -0500

    SEO optimization: add react-helmet-async, meta tags, sitemap, robots.txt, and canonical URL redirect

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import SEO from './components/SEO';
 
 const Layout = ({ children }) => {
     return (
         <div>
             <Navbar />
+            <SEO />
             <div className="content">{children}</div>
         </div>
     );
