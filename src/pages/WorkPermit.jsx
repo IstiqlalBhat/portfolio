c878a9b75a7479884dc0f9204cc1c13ae9fe54f9
Istiqlal Aurangzeb <istiqlal.aurangzeb@gmail.com>
2025-12-05 00:15:34 -0500

    Text Change 3

--- src/pages/WorkPermit.jsx
+++ src/pages/WorkPermit.jsx
@@ -7,7 +7,7 @@ const WorkPermit = () => {
             <div className="work-permit-card">
                 <h2 className="work-permit-headline">🎓 Work Permit</h2>
                 <p className="work-permit-summary">
-                    I'm currently on <strong>F1 OPT</strong> 🇺🇸. My STEM OPT is set to expire on <strong>January 2027</strong> 📅.
+                    I'm currently on <strong>F1 OPT</strong>. My STEM OPT is set to expire on <strong>January 2027</strong> 📅.
                     I am eligible for a STEM OPT extension, and subsequently may need H1B sponsorship. 🌟
                 </p>
             </div>
