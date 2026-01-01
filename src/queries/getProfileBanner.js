0e811108890628bd4198724c9b89649c4df2420b
Istiqlal Aurangzeb <istiqlal.aurangzeb@gmail.com>
2026-01-01 16:29:44 -0500

    Profil Desciprtion Chnaged

--- src/queries/getProfileBanner.js
+++ src/queries/getProfileBanner.js
@@ -18,21 +18,17 @@ const GET_PROFILE_BANNER = `
 `;
 
 export async function getProfileBanner() {
-    try {
-        // const data = await datoCMSClient.request(GET_PROFILE_BANNER);
-        // return data.profilebanner;
-        throw new Error("Using mock data");
-    } catch (error) {
-        return {
-            headline: "Istiqlal Aurangzeb",
-            profileSummary: `Research Software Developer at Clemson University bridging AI and blockchain with construction technology.
-             Published peer-reviewed research on eradicating procurement fraud and automating emissions tracking with 98% accuracy. Built systems delivering subcontractor procurement fraud reduction, 37% lower smart contract costs, and sub-200ms query speeds. 
-             Proficient in Solidity, multimodal AI, and full-stack development. 
-             Driven to transform the $13 trillion construction industry by building and shipping measurable solutions. 
-             Completing a Master’s in Computer Science and seeking roles to apply AI/blockchain expertise in construction tech.`,
-            linkedinLink: "https://www.linkedin.com/in/istiqlalbhat/",
-            resumeLink: { url: "#" }, // TODO: Add actual resume link if available
-            backgroundImage: { url: "https://wallpapers.com/images/hd/netflix-background-gs7hjuwvv2g0e9fj.jpg" }
-        };
-    }
+  try {
+    // const data = await datoCMSClient.request(GET_PROFILE_BANNER);
+    // return data.profilebanner;
+    throw new Error("Using mock data");
+  } catch (error) {
+    return {
+      headline: "Istiqlal Aurangzeb",
+      profileSummary: ` Software Developer at Clemson University.`,
+      linkedinLink: "https://www.linkedin.com/in/istiqlalbhat/",
+      resumeLink: { url: "#" }, // TODO: Add actual resume link if available
+      backgroundImage: { url: "https://wallpapers.com/images/hd/netflix-background-gs7hjuwvv2g0e9fj.jpg" }
+    };
+  }
 }
